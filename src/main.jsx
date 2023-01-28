b99c30cee69a1e7aa02e92cb5c3abefd148d31ab
Aine Doyle <ainedoyleAD@gmail.com>
2023-01-28 15:55:14 +0000

    added dynamic import for App

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './app/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import SuspenseLoader from './components/SuspenseLoader';
+
+const App = lazy(() => import("./App"));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Suspense fallback={<SuspenseLoader />}>
+            <Routes>
+              <Route path="/*" element={<App />} />
+            </Routes>
+        </Suspense>
+      </BrowserRouter>
     </PersistGate>
   </Provider>,
 );
